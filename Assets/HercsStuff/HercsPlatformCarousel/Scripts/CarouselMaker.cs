abf57b2858c325c25a645d6001966f9005b7c2a3
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2020-05-05 12:50:25 -0400

    Added time mechanics initial setup. Not scene-implemented yet

--- Assets/HercsStuff/HercsPlatformCarousel/Scripts/CarouselMaker.cs
+++ Assets/HercsStuff/HercsPlatformCarousel/Scripts/CarouselMaker.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TimeManipuation;
 public class CarouselMaker : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed;
