06127e83a29459265f8d2eef24792ecb2af13b86
Blair-White <blairjwhite@hotmail.com>
2020-05-10 14:51:54 -0400

    IsoDemo Working

--- Assets/Blair/IsoMetricDemo/IsoCamera.cs
+++ Assets/Blair/IsoMetricDemo/IsoCamera.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class IsoCamera : MonoBehaviour
 {
+
+    public GameObject CameraTarget;
+    enum CameraStates
+    {
+        Default, 
+        RotateMapCCW, //This is to divide the stage into different sections.
+        RotateMapCW,  
+        CinematicA, 
+        Shoulder, 
+    };
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Blair/IsoMetricDemo/IsoPlayerMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsoPlayerMove : MonoBehaviour
+{
+    float moveSpeed = 4f;
+    Vector3 forward, right;
+    PlayerInputAction controls;
+    float xAxis, yAxis;
+    public Vector2 movementInput;
+    public float Hmovement, Vmovement;
+    void Awake()
+    {
+        controls = new PlayerInputAction();
+        controls.PlayerControls.Move.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
+        controls.PlayerControls.Move.canceled += ctx => movementInput = Vector2.zero;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        Hmovement = movementInput.x; Vmovement = movementInput.y;
+        Vector3 direction = new Vector3(Hmovement, 0, Vmovement);
+        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Hmovement;
+        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Vmovement;
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+        
+        
+        transform.position += rightMovement;
+        transform.position += upMovement;
+        transform.forward = heading;
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
