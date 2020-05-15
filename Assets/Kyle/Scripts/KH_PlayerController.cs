decd50eb9e34eba726b0c8f9dd577441a1f9c59c
KojimaMcMaple <KyleHunter.KH@outlook.com>
2020-05-15 02:22:26 -0400

    finished Jump, convert AddForce to rb.velocity

--- Assets/Kyle/Scripts/KH_PlayerController.cs
+++ Assets/Kyle/Scripts/KH_PlayerController.cs
@@ -9,6 +9,7 @@ public class KH_PlayerController : MonoBehaviour
     public float jumpForce = 7.0f; //from https://youtu.be/vdOFUFMiPDU (How To Jump in Unity - Unity Jumping Tutorial | Make Your Characters Jump in Unity)
     public float fallMultiplier = 2.5f; //from https://youtu.be/7KiK0Aqtmzc (Better Jumping in Unity With Four Lines of Code)
     public float lowJumpMultiplier = 2.0f;
+    public LayerMask groundLayers;
     private Vector3 forward, right;
     private PlayerInputAction controls;
     private Vector2 movementInput;
@@ -17,7 +18,7 @@ public class KH_PlayerController : MonoBehaviour
     private GameObject mPlayer;
     private Animator anim;
     private Rigidbody rb;
-    private SphereCollider playerCollider;
+    private BoxCollider playerCollider;
 
     void Awake()
     {
@@ -37,7 +38,7 @@ public class KH_PlayerController : MonoBehaviour
         mPlayer = GameObject.Find("Character");
         anim = mPlayer.GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
-        playerCollider = GetComponent<SphereCollider>();
+        playerCollider = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
@@ -50,7 +51,7 @@ public class KH_PlayerController : MonoBehaviour
     {
         horizontalMovement = movementInput.x;
         verticalMovement = movementInput.y;
-        
+
         //Vector3 rightMovement = right * moveSpeed * Time.deltaTime * horizontalMovement;
         //Vector3 upMovement = forward * moveSpeed * Time.deltaTime * verticalMovement;
         //Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
@@ -58,12 +59,40 @@ public class KH_PlayerController : MonoBehaviour
         //transform.position += rightMovement;
         //transform.position += upMovement;
 
-        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * horizontalMovement;
-        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * verticalMovement;
-        Vector3 newMovement = rightMovement + upMovement;
-        Vector3 heading = Vector3.Normalize(newMovement);
+        //Vector3 rightMovement = right * moveSpeed * Time.deltaTime * horizontalMovement;
+        //Vector3 upMovement = forward * moveSpeed * Time.deltaTime * verticalMovement;
+        Vector3 rightMovement = right * moveSpeed * horizontalMovement;
+        Vector3 upMovement = forward * moveSpeed * verticalMovement;
+        Vector3 groundMovement = rightMovement + upMovement;
+        Vector3 heading = Vector3.Normalize(groundMovement);
 
-        rb.MovePosition(transform.position + newMovement);
+        //rb.MovePosition(transform.position + movement);
+        //rb.AddForce(movement, ForceMode.Acceleration);
+        rb.velocity = new Vector3(groundMovement.x, rb.velocity.y, groundMovement.z);
+
+        // JUMPING
+        if (IsGrounded() && jumpInput)
+        {
+            rb.velocity = Vector3.up * jumpForce;
+        }
+        
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * fallMultiplier * Time.deltaTime; //using Time.deltaTime due to acceleration
+        }
+        else if (rb.velocity.y > 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * lowJumpMultiplier * Time.deltaTime; //using Time.deltaTime due to acceleration
+        }
+
+        // FULL STOP WHEN JOYSTICK IS RELEASED
+        if (horizontalMovement == 0 && verticalMovement == 0)
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+
+        // DISABLE RIGIDBODY FUMBLING
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
 
         if (horizontalMovement != 0 || verticalMovement != 0)
         {
@@ -73,9 +102,13 @@ public class KH_PlayerController : MonoBehaviour
         else
         {
             anim.SetFloat("HSpeed", 0);
-
         }
       
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, playerCollider.bounds.extents.y + 0.1f, groundLayers);
     }
 
     private void OnEnable()
