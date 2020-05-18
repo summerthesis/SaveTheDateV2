f391e13c117c668963d1d2121efb73c3a5cb1375
Blair-White <blairjwhite@hotmail.com>
2020-05-18 13:06:56 -0400

    f

--- Assets/Blair/IsoMetricDemo/CameraTarget.cs
+++ Assets/Blair/IsoMetricDemo/CameraTarget.cs
@@ -8,7 +8,7 @@ public class CameraTarget : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        mPlayer = GameObject.FindGameObjectWithTag("Player");   
     }
 
     // Update is called once per frame
