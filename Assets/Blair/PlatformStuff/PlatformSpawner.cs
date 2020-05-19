5b4adecc54fe8b20fd675524f5490877c50faf28
Blair-White <blairjwhite@hotmail.com>
2020-05-18 20:28:19 -0400

    Infinite spawner/time mech

--- Assets/Blair/PlatformStuff/PlatformSpawner.cs
+++ Assets/Blair/PlatformStuff/PlatformSpawner.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlatformSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Despawner;
+    public GameObject SimplePlatformPrefab;
+    public float PlatformMoveSpeed;
+    public int Interval;
+    private int Count;
+    private Vector3 Forward;
+    private GameObject NewPlatform;
     void Start()
     {
-        
+        Forward = this.transform.forward;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Count++;
+        if (Count >= Interval)
+        {
+            Count = 0;
+            NewPlatform = Instantiate(SimplePlatformPrefab, this.transform.position, Quaternion.identity);
+            NewPlatform.GetComponent<PlatformStraight>().mSpeed = PlatformMoveSpeed;
+        }
     }
 }
---
+++ Assets/Blair/PlatformStuff/PlatformStraight.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformStraight : MonoBehaviour
+{
+    private float  SlowedSpeed, FastSpeed;
+    public float mSpeed, NormalSpeed;
+
+    void Start()
+    {
+        transform.rotation = new Quaternion(0, 0, 0, 0);
+        
+        SlowedSpeed = mSpeed / 2;
+        FastSpeed = mSpeed * 2;
+        NormalSpeed = mSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * Time.deltaTime * mSpeed;
+    }
+
+     void TimeSlow()
+    {
+        mSpeed = SlowedSpeed;
+    }
+    void TimeStop()
+    {
+        mSpeed = 0;
+    }
+    void TimeFastForward()
+    {
+        mSpeed = FastSpeed;
+    }
+    void JumpForward()
+    {
+        transform.Translate(-transform.right * 4);
+    }
+    void RestoreToNormal()
+    {
+        mSpeed = NormalSpeed;
+    }
+    Vector3 GetLocalDirection(Transform transform, Vector3 destination)
+    {
+        return transform.InverseTransformDirection((destination - transform.position).normalized);
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Despawner")
+        {  
+            Destroy(this.gameObject);
+            Debug.Log("Collided");
+        }
+    }
+}
