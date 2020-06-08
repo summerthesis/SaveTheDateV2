6fd81923e3bfb9a618bcdb95dc0a41a014046ef0
Shu Deng <dengshu9127@gmail.com>
2020-06-08 14:47:59 -0400

    added Interactable.cs and CameraController.cs(for test scene)

---
+++ Assets/Shu Deng (Mike)/Scripts/CameraControll.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    public bool moveCamera = true;
+    public float smoothing = 7f;
+    public Vector3 offset = new Vector3(0f, 1.5f, 0f);
+    public Transform playerPosition;
+
+    // Start is called before the first frame update
+    private IEnumerator Start()
+    {
+        // If the camera shouldn't move, do nothing.
+        if (!moveCamera)
+            yield break;
+
+        // Wait a single frame to ensure all other Starts are called first.
+        yield return null;
+
+        // Set the rotation of the camera to look at the player's position with a given offset.
+        transform.rotation = Quaternion.LookRotation(playerPosition.position - transform.position + offset);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (!moveCamera)
+            return;
+
+        // Find a new rotation aimed at the player's position with a given offset.
+        Quaternion newRotation = Quaternion.LookRotation(playerPosition.position - transform.position + offset);
+
+        // Spherically interpolate between the camera's current rotation and the new rotation.
+        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * smoothing);
+    }
+}
---
+++ Assets/Shu Deng (Mike)/Scripts/Interactable.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public GameObject PopupPrefab;
+    public Vector3 PopupPosition;
+    public bool PopupInWorld = false;
+    bool m_PopupShowed = false;
+    GameObject m_Popup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<BoxCollider>().isTrigger = true;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<KH_PlayerController>() != null)
+        {
+            if (m_PopupShowed == false)
+            {
+                m_Popup = Instantiate(PopupPrefab, this.transform);
+                if (PopupInWorld == true)
+                {
+                    m_Popup.transform.position = PopupPosition;
+                }
+                else
+                {
+                    m_Popup.transform.localPosition = PopupPosition;
+                }               
+                m_PopupShowed = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<KH_PlayerController>() != null)
+        {
+            if (m_PopupShowed == true)
+            {
+                Destroy(m_Popup);
+                m_PopupShowed = false;
+            }
+        }
+    }
+}
