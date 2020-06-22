f68fca209f59858f26599488dd703d8a52d72d83
Shu Deng <dengshu9127@gmail.com>
2020-06-22 14:27:04 -0400

    updated DrawToDrop.cs to draw a line between start and end

--- Assets/Shu Deng (Mike)/Scripts/Editor/DrawToDropObjects.cs
+++ Assets/Shu Deng (Mike)/Scripts/Editor/DrawToDropObjects.cs
@@ -6,7 +6,7 @@ using UnityEditor;
 public class DrawToDropObjects : EditorWindow
 {
     private GameObject m_ObjectToDrop;
-    private Transform m_StartPoint, m_EndPoint;
+    private Transform m_StartTransform, m_EndTransform;
     private float m_Spacing;
     private int m_Index;
 
@@ -19,30 +19,38 @@ public class DrawToDropObjects : EditorWindow
         window.Show();
     }
 
+    private void Update()
+    {
+        if (m_StartTransform != null && m_EndTransform != null)
+        {
+            Debug.DrawLine(m_StartTransform.position, m_EndTransform.position, Color.white);
+        }
+    }
+
     void OnGUI()
     {
         GUILayout.Label("Game Object Prefab", EditorStyles.boldLabel);
         m_ObjectToDrop = (GameObject)EditorGUILayout.ObjectField("Object To Drop", m_ObjectToDrop, typeof(GameObject), false);
 
         GUILayout.Label("Game Object Layout", EditorStyles.boldLabel);
-        m_StartPoint = (Transform)EditorGUILayout.ObjectField("Start Location", m_StartPoint, typeof(Transform), true);
-        m_EndPoint = (Transform)EditorGUILayout.ObjectField("End Location", m_EndPoint, typeof(Transform), true);
-        m_Spacing = EditorGUILayout.FloatField("Object Spacing", m_Spacing);
+        m_StartTransform = (Transform)EditorGUILayout.ObjectField("Start Location", m_StartTransform, typeof(Transform), true);
+        m_EndTransform = (Transform)EditorGUILayout.ObjectField("End Location", m_EndTransform, typeof(Transform), true);
+        m_Spacing = EditorGUILayout.FloatField("Object Spacing", m_Spacing);            
 
         GUILayout.Label("Game Object Properties", EditorStyles.boldLabel);
         m_Index = EditorGUILayout.IntField("Starting Index", m_Index);
 
         if (GUILayout.Button("Create Objects"))
         {
-            Vector3 displacement = (m_EndPoint.position - m_StartPoint.position).normalized * m_Spacing;
-            int numbers = (int)((m_EndPoint.position - m_StartPoint.position).magnitude / m_Spacing);
+            Vector3 displacement = (m_EndTransform.position - m_StartTransform.position).normalized * m_Spacing;
+            int numbers = (int)((m_EndTransform.position - m_StartTransform.position).magnitude / m_Spacing);
             for (int i = 0; i < numbers; ++i)
             {
                 GameObject temp = (GameObject)PrefabUtility.InstantiatePrefab(m_ObjectToDrop);
                 temp.SetActive(true);
                 temp.name = temp.name + "_" + m_Index;
                 ++m_Index;
-                temp.transform.position = m_StartPoint.position + displacement * i;
+                temp.transform.position = m_StartTransform.position + displacement * i;
             }            
         }        
     }
