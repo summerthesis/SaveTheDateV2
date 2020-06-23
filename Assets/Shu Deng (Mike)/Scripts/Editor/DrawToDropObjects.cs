f4b201474750d763e430f0ed4a1ded1ba6116bb8
Shu Deng <dengshu9127@gmail.com>
2020-06-23 18:05:38 -0400

    updated DrawToDrop

--- Assets/Shu Deng (Mike)/Scripts/Editor/DrawToDropObjects.cs
+++ Assets/Shu Deng (Mike)/Scripts/Editor/DrawToDropObjects.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System;
 
 public class DrawToDropObjects : EditorWindow
 {
     private GameObject m_ObjectToDrop;
-    private Transform m_StartTransform, m_EndTransform;
+    private Transform m_StartTransform, m_EndTransform, m_GroupTransform;
     private float m_Spacing;
     private int m_Index;
+        
 
     // Add menu named "My Window" to the Window menu
     [MenuItem("Window/DrawToDropObjects")]
@@ -24,11 +26,11 @@ public class DrawToDropObjects : EditorWindow
         if (m_StartTransform != null && m_EndTransform != null)
         {
             Debug.DrawLine(m_StartTransform.position, m_EndTransform.position, Color.white);
-        }
+        }        
     }
 
-    void OnGUI()
-    {
+    public void OnGUI()
+    {       
         GUILayout.Label("Game Object Prefab", EditorStyles.boldLabel);
         m_ObjectToDrop = (GameObject)EditorGUILayout.ObjectField("Object To Drop", m_ObjectToDrop, typeof(GameObject), false);
 
@@ -40,8 +42,19 @@ public class DrawToDropObjects : EditorWindow
         GUILayout.Label("Game Object Properties", EditorStyles.boldLabel);
         m_Index = EditorGUILayout.IntField("Starting Index", m_Index);
 
-        if (GUILayout.Button("Create Objects"))
+        if (GUILayout.Button("Create Objects Group"))
         {
+            // Create parent
+            GameObject parent = new GameObject();            
+            parent.name = "Group_" + m_ObjectToDrop.name + "_" + m_Index;
+            parent.transform.position = (m_StartTransform.position + m_EndTransform.position) * 0.5f;
+
+            m_StartTransform = Instantiate(m_StartTransform.gameObject, m_StartTransform.position, m_StartTransform.rotation).transform;
+            m_StartTransform.SetParent(parent.transform, true);
+            m_EndTransform = Instantiate(m_EndTransform.gameObject, m_EndTransform.position, m_EndTransform.rotation).transform;
+            m_EndTransform.SetParent(parent.transform, true);
+
+            // Create target objects
             Vector3 displacement = (m_EndTransform.position - m_StartTransform.position).normalized * m_Spacing;
             int numbers = (int)((m_EndTransform.position - m_StartTransform.position).magnitude / m_Spacing);
             for (int i = 0; i < numbers; ++i)
@@ -51,7 +64,40 @@ public class DrawToDropObjects : EditorWindow
                 temp.name = temp.name + "_" + m_Index;
                 ++m_Index;
                 temp.transform.position = m_StartTransform.position + displacement * i;
+                temp.transform.SetParent(parent.transform, true);
+            }
+            m_Index -= numbers;
+            m_GroupTransform = parent.transform;
+        }
+
+        GUILayout.Label("Objects Group Update", EditorStyles.boldLabel);
+        m_GroupTransform = (Transform)EditorGUILayout.ObjectField("Objects Group", m_GroupTransform, typeof(Transform), true);
+
+        EditorGUILayout.HelpBox("Make sure the correct group object is set before updating!", MessageType.Warning, true);
+
+        if (GUILayout.Button("Update Objects Group"))
+        {
+            m_StartTransform = m_GroupTransform.GetChild(0);
+            m_EndTransform = m_GroupTransform.GetChild(1);
+
+            int numOfChildren = m_GroupTransform.childCount;
+            for (int i = 2; i < numOfChildren; ++i)
+            {
+                DestroyImmediate(m_GroupTransform.GetChild(2).gameObject);
             }            
-        }        
+
+            Vector3 displacement = (m_EndTransform.position - m_StartTransform.position).normalized * m_Spacing;
+            int numbers = (int)((m_EndTransform.position - m_StartTransform.position).magnitude / m_Spacing);
+            for (int i = 0; i < numbers; ++i)
+            {
+                GameObject temp = (GameObject)PrefabUtility.InstantiatePrefab(m_ObjectToDrop);
+                temp.SetActive(true);
+                temp.name = temp.name + "_" + m_Index;
+                ++m_Index;
+                temp.transform.position = m_StartTransform.position + displacement * i;
+                temp.transform.SetParent(m_GroupTransform, true);
+            }
+            m_Index -= numbers;
+        }
     }
 }
