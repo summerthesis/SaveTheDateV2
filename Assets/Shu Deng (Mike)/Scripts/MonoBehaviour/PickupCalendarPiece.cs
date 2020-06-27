e264072a48010d3a6ad7b92236db94b158d1f1f2
Shu Deng <dengshu9127@gmail.com>
2020-06-27 01:16:39 -0400

    added Mayan calendar collection base mechanism

---
+++ Assets/Shu Deng (Mike)/Scripts/MonoBehaviour/PickupCalendarPiece.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Pickup))]
+public class PickupCalendarPiece : MonoBehaviour
+{
+    public int pieceNumber = 1;
+
+    void OnPickedUp()
+    {
+        FindObjectOfType<CalendarPieceHUDController>().PieceCollected(pieceNumber);
+    }
+}
