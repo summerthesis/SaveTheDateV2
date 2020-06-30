34e6e4cbc93d802b453f1049bf778a06b1a597c1
Shu Deng <dengshu9127@gmail.com>
2020-06-30 19:18:55 -0400

    finished TimeInteractable objects with highlights

--- Assets/_CodingStandard/TimeController.cs
+++ Assets/_CodingStandard/TimeController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,7 +9,7 @@ public class TimeController : MonoBehaviour
     PlayerInputAction controls;
     private float Energy;
     private float MaxEnergy = 1000;
-    private float MaxCastRange = 1000;
+    private float MaxCastRange = 15;
     public float EnergyCost = 9;
     public float StopCost = 750;
     private int Count;  
@@ -17,40 +18,63 @@ public class TimeController : MonoBehaviour
     //private GameObject EnergyBar;
     private bool Stopping;
 
-    // Added by Shu Deng (Mike)
-    public EnergyBarController EnergyBarController;
-    
     private enum TimeStates
     {
+        Unassigned,
         Available,
-        Slowing, 
+        Slowing,
         FastForwarding
     }
     private TimeStates TimeState = TimeStates.Available;
+
+    // Added by Shu Deng (Mike)
+    public EnergyBarController EnergyBarController;
+    [ColorUsage(true, true)]
+    public Color HighlightFastforward, HighlightNeutral, HighlightPause, HighlightSlow;
+    private int ShaderIDColor, ShaderIDIsTwinkling, ShaderIDIsHighlighted;
+    private GameObject[] AllTimeTaggedObjects;    
+    
     void Awake()
     {
         SetupControls();
-        Player = GameObject.FindGameObjectWithTag("Player");
-        //EnergyBar = GameObject.FindGameObjectWithTag("EnergyBar");
+        Player = GameObject.FindGameObjectWithTag("Player");        
+
+        // Added by Shu Deng (Mike)
+        AllTimeTaggedObjects = GameObject.FindGameObjectsWithTag("TimeInteractable");
+        ShaderIDColor = Shader.PropertyToID("Color_5D1C9DC");
+        ShaderIDIsTwinkling = Shader.PropertyToID("Boolean_CFDDD5C1");
+        ShaderIDIsHighlighted = Shader.PropertyToID("Boolean_82F39996");
     }
 
     void Update()
     {
+        // Added by Shu Deng (Mike)
+        TimeTaggedObjects = FindObjectsWithinRange().ToArray();
+
         switch (TimeState)
         {
             case TimeStates.Available:
                 Energy += 4; if (Energy > MaxEnergy) Energy = MaxEnergy;
                 SetEnergyBarScale();
+
+                // Added by Shu Deng (Mike)
+                ChangeHightlightColor(HighlightNeutral, 1);
                 break;
 
             case TimeStates.Slowing:
                 Energy -= EnergyCost; if (Energy < 0) { Energy = 0; EndSlow(); }
                 SetEnergyBarScale();
+
+                // Added by Shu Deng (Mike)
+                ChangeHightlightColor(HighlightSlow, 0);
                 break;
 
             case TimeStates.FastForwarding:
                 Energy -= EnergyCost; if (Energy < 0) { Energy = 0; EndFastForward(); }
                 SetEnergyBarScale();
+
+                // Added by Shu Deng (Mike)
+                ChangeHightlightColor(HighlightFastforward, 0);
                 break;
 
             default:
@@ -59,27 +83,38 @@ public class TimeController : MonoBehaviour
         if(Stopping)
         {
             Count++; 
-            if(Count > 120) { Stopping = false; Count = 0; LoopThroughObjects("RestoreToNormal", false); }
+            if(Count > 120) 
+            { 
+                Stopping = false; 
+                Count = 0; 
+                LoopThroughObjects("RestoreToNormal", false);
+                TimeState = TimeStates.Available;
+            }
+
+            // Added by Shu Deng (Mike)
+            ChangeHightlightColor(HighlightPause, 0);
         }
     }
     void LoopThroughObjects(string SendThisMessage, bool CheckDistance)
     {
-        TimeTaggedObjects = GameObject.FindGameObjectsWithTag("TimeInteractable");
-
         if (TimeTaggedObjects.Length > 0)
             foreach (GameObject obj in TimeTaggedObjects)
             {
-            float distance = Vector3.Distance(Player.transform.position, obj.transform.position);
-            if (CheckDistance)
-            {
-                if (distance < MaxCastRange)
-                obj.gameObject.SendMessage(SendThisMessage);
-            }
-            else
-                obj.gameObject.SendMessage(SendThisMessage);
+                /*
+                float distance = Vector3.Distance(Player.transform.position, obj.transform.position);
+                if (CheckDistance)
+                {
+                    if (distance < MaxCastRange)
+                    obj.gameObject.SendMessage(SendThisMessage);
+                }
+                else
+                    obj.gameObject.SendMessage(SendThisMessage);
+                */
+
+                // Added by Shu Deng (Mike)
+                obj.SendMessage(SendThisMessage);
             }
     }
-
 
     void Slow()
     {
@@ -140,7 +175,11 @@ public class TimeController : MonoBehaviour
     }
     void SetupControls()
     {
-        controls = new PlayerInputAction();
+        // controls = new PlayerInputAction();
+
+        // Added by Shu Deng (Mike)
+        controls = InputManagerSingleton.Instance;
+
         controls.TimeControls.TimeFastForward.performed += ctx => FastForward();
         controls.TimeControls.TimeFastForward.canceled += ctx => EndFastForward();
         controls.TimeControls.TimeSlow.performed += ctx => Slow();
@@ -153,11 +192,59 @@ public class TimeController : MonoBehaviour
     void SetEnergyBarScale()
     {
         float EnergyBarScale = Energy / MaxEnergy;
-        //EnergyBar.transform.localScale = new Vector3(EnergyBarScale, 1, 1);
-
+        
         // Added by Shu Deng (Mike)
         EnergyBarController.UpdateEnergyBar(EnergyBarScale);
     }
     private void OnEnable() { controls.Enable(); }
     private void OnDisable() { controls.Disable(); }
+
+    // Added by Shu Deng (Mike), needs space querying optimization in the future
+    IEnumerable<GameObject> FindObjectsWithinRange()
+    {
+        foreach (var member in AllTimeTaggedObjects)
+        {
+            if (Vector3.Distance(Player.transform.position, member.transform.position) < MaxCastRange)
+            {
+                yield return member;
+            }
+            else
+            {
+                MeshRenderer[] meshRenderers = member.GetComponentsInChildren<MeshRenderer>();
+                if (meshRenderers != null)
+                {
+                    foreach (var meshRenderer in meshRenderers)
+                    {
+                        Material[] materials = meshRenderer.materials;
+                        foreach (var material in materials)
+                        {
+                            material.SetFloat(ShaderIDIsHighlighted, 0);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Added by Shu Deng (Mike)
+    void ChangeHightlightColor(Color destinationColor, int isTwinkling)
+    {
+        foreach (GameObject obj in TimeTaggedObjects)
+        {
+            MeshRenderer[] meshRenderers = obj.GetComponentsInChildren<MeshRenderer>();
+            if (meshRenderers != null)
+            {
+                foreach (var meshRenderer in meshRenderers)
+                {
+                    Material[] materials = meshRenderer.materials;
+                    foreach (var material in materials)
+                    {
+                        material.SetFloat(ShaderIDIsHighlighted, 1);
+                        material.SetColor(ShaderIDColor, destinationColor);
+                        material.SetInt(ShaderIDIsTwinkling, isTwinkling);
+                    }
+                }
+            }
+        }
+    }
 }
