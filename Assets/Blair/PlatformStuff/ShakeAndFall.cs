68324ca34577bc25e98b18d0d569100404b4b404
Blair-White <blairjwhite@hotmail.com>
2020-07-14 13:25:38 -0400

    asdf

--- Assets/Blair/PlatformStuff/ShakeAndFall.cs
+++ Assets/Blair/PlatformStuff/ShakeAndFall.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class ShakeAndFall : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject mPlayer;
+    private int RespawnDuration, respawnCount;
+    private bool Activated, Removed;
+    private DOTweenAnimation mTween;
     void Start()
     {
-        
+        mPlayer = GameObject.FindGameObjectWithTag("Player");
+        mTween = this.GetComponent<DOTweenAnimation>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if(Activated)
+        {
+            respawnCount++;
+            if(respawnCount > RespawnDuration)
+            {
+                
+            }
+
+        }
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject == mPlayer)
+        {
+            if(mPlayer.transform.position.y > this.transform.position.y)
+            {
+                Debug.Log("Player Landed on Shake and Fall Platform");
+                this.DOPlay();
+                
+            }
+        }
+    
     }
 }
